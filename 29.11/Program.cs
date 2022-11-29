1e85d01421397eb2a3522092eb10c8c2028592d7
razansmadi99@gmail.com <razansmadi99@gmail.com>
2022-11-29 18:23:57 +0300

    29.11

--- 29.11/Program.cs
+++ 29.11/Program.cs
@@ -1,47 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _29._11
+namespace _29_11
 {
-
-    public class Employee
+    class Employee
     {
-        public string name { set; get; }
-        public int birth { set; get; }
-        public int ID { set; get; }
-        public  Employee(string name,  int birth, int ID)
+        public string name { get; set; }
+        public int id { get; set; }
+        public DateTime dateofbirth { get; set; }
+        public Employee(string name, int id, int day, int month, int year)
         {
-
             this.name = name;
-            this.birth = birth;
-            this.ID = ID;
-        }
-
-        public virtual void calcAge(int year)
-        {
+            this.id = id;
+            dateofbirth = new DateTime(year, month, day);
 
         }
-        public void test()
+
+        public void age()
         {
-            Console.WriteLine($" Name:  {name} , BIRTH DAY: {birth} , ID: {ID}");
+            DateTime now = DateTime.Now; TimeSpan value = now.Subtract(dateofbirth);
+
+            Console.WriteLine((int)value.TotalDays / 365);
+
         }
+        public virtual void print() { Console.WriteLine(name + " " + id + " " + dateofbirth); }
 
     }
-    class Manger: Employee
+    class Manager : Employee
     {
-
-        
-        public Manger(string name, int birth,int id ) : base(name,birth,id)
+        public Manager(string name, int id, int day, int month, int year) : base(name, id, day, month, year)
         {
-            
-
         }
-        public override void calcAge(int year)
+        public override void print()
         {
-           Console.WriteLine(  2022 - year);
+            Console.WriteLine(name + " " + id + " ");
         }
 
     }
@@ -49,10 +45,10 @@ namespace _29._11
     {
         static void Main(string[] args)
         {
-            Manger m = new Manger("razan", 1999 , 999);
-            m.test();
-            m.calcAge(1999);
 
+            Manager manger = new Manager("razan", 99, 3, 11, 1999);
+            manger.print();
+            manger.age();
         }
     }
 }
